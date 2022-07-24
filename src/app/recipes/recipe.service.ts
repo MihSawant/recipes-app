072ef95ef963b027cbac41ab48f3341268429cca
Mihir Sawant <mih3190Sawant@outlook.com>
2022-07-24 12:23:11 +0530

    modified the reicpes class to add the ingredients of recipe.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,24 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipes.model";
 
 @Injectable()
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is just a test desc',
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/10_minute_Recipe_for_a_Healthy_Garden_Salad_-_49859886512.jpg/800px-10_minute_Recipe_for_a_Healthy_Garden_Salad_-_49859886512.jpg?20201125214626'),
-        new Recipe('A Test Two Recipe', 'This is just a test desc for Two',
-            'https://static.toiimg.com/photo/53096885.cms')
+        new Recipe('Vegetable Salad', 'Mix Vegetble Salad, Perfect for Healthy Breakfast',
+            'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDgyMS1wYWxlby1icmVha2Zhc3QzLmpwZw.jpg?s=rTT3PeFnfont5Oq7MVQpVTpRDdkiTFqvAVG30Kkz3q4',
+            [
+                new Ingredient('Cucumber', 2),
+                new Ingredient('Carrot', 1),
+                new Ingredient('Tomato', 1),
+                new Ingredient('Eggs', 1)
+            ]),
+        new Recipe('Aloo Paratha', 'Great and Wholesome breakfast',
+            'https://live.staticflickr.com/1548/24330115331_f731b72a75_b.jpg',
+            [
+                new Ingredient('Potato', 3),
+                new Ingredient('Wheat Flour', 1)
+            ])
     ];
 
     recipeSelected = new EventEmitter<Recipe>();
